be34547e1dbedd12a3d5848db3fe8982c3a0b6e1
Santhosh16s <code88763@gmail.com>
2025-07-31 15:22:38 +0530

    Merge GitHub changes

--- main.py
+++ main.py
@@ -801,11 +801,13 @@ async def start_with_payload(update: Update, context: ContextTypes.DEFAULT_TYPE)
                     f"👉 <a href='{PRIVATE_CHANNEL_LINK}'>SK Movies Updates (News)🔔</a> - புதிய படங்கள், அப்டேட்கள் அனைத்தும் இங்கே கிடைக்கும்.\nJoin பண்ணுங்க!\n\n"
                     f"⚠️ இந்த File 10 நிமிடங்களில் நீக்கப்படும். தயவுசெய்து இந்த File ஐ உங்கள் saved messages க்கு அனுப்பி வையுங்கள்."
                 )
-                sent_msg = await update.message.reply_document(
+                sent_msg = await context.bot.send_document(
+                    chat_id=update.effective_chat.id,  # message வந்த இடத்தில் file போடுவோம்
                     document=file_id_to_send,
                     caption=caption,
                     parse_mode="HTML"
                 )
+
                 await update.message.reply_text("✅ உங்கள் கோப்பு இங்கே!")
                 asyncio.create_task(delete_after_delay(context, sent_msg.chat.id, sent_msg.message_id))
 
